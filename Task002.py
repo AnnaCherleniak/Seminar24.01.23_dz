ba1c60fbbe6f20b07e19652a52e08ad4a217673f
Anna <ankakras@yandex.ru>
2023-01-31 16:28:18 +0700

    Solved task002

--- Task002.py
+++ Task002.py
@@ -5,3 +5,36 @@
 # функция кодирует сдвиг алфавита на 3 позиции:
 
 
+def encrypt_caesar(msg, shift=3):
+    new_msg = ''
+    alphabet = [chr(i) for i in range(ord('а'), ord('я') + 1)]
+    Alphabet = [chr(i) for i in range(ord('А'), ord('Я') + 1)]
+    for el in msg:
+        if el in alphabet:
+            new_msg += chr(ord(el) + shift)
+        elif el in Alphabet:
+            new_msg += chr(ord(el) + shift)
+        else:
+            new_msg += el        
+    return new_msg
+
+
+def decrypt_caesar(msg, shift=3):
+    new_msg = ''
+    alphabet = [chr(i) for i in range(ord('а'), ord('я') + 1)]
+    Alphabet = [chr(i) for i in range(ord('А'), ord('Я') + 1)]
+    for el in msg:
+        if el in alphabet:
+            new_msg += chr(ord(el) - shift)
+        elif el in Alphabet:
+            new_msg += chr(ord(el) - shift)
+        else:
+            new_msg += el        
+    return new_msg
+
+
+msg = 'Да здравструет салат Цезарь!'
+encrypt = encrypt_caesar(msg)
+print(encrypt)
+decrypt = decrypt_caesar(encrypt)
+print(decrypt)
